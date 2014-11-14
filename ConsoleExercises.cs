f4ead60159d0c10777eb51257fd0f34004e51928
lleaff <lleaff@users.noreply.github.com>
2014-11-14 22:07:59 +0100

    Yo.

---
+++ ConsoleExercises.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FundamentalsOfCExercisesVariablesChapter
+{
+
+	public class ConsoleExercises {
+
+		public ConsoleExercises ()
+		{
+		}
+
+		private static bool YesNo() {
+			Console.WriteLine ("Y/N?");
+			char yesno = (char)Console.Read();
+			Console.WriteLine ();
+			return (yesno == 'y' || yesno == 'Y');
+			}
+
+
+		public static void DoExercises() {
+
+			#region Exercise 2
+			Exercises.Title ("2");
+			if(YesNo()) {
+
+				Console.WriteLine ("First name?");
+				string inputFirstName = Console.ReadLine ();
+				Console.WriteLine ("Last name?");
+				string inputLastName = Console.ReadLine ();
+				Console.WriteLine ("Phone number?");
+				int inputPhone = int.Parse (Console.ReadLine ());
+				Console.WriteLine ("Address?");
+				string inputAddress = Console.ReadLine ();
+				Console.WriteLine ("Profession?");
+				string inputProfession = Console.ReadLine ();
+
+
+
+				Console.Write ("{0, -10}", inputFirstName);
+				Console.Write (" {0, -10}", inputLastName);
+				Console.Write (" {0, 10}{1}", inputPhone, Environment.NewLine);
+				Console.Write (inputAddress);
+				Console.Write ("{0, -15}", inputProfession);
+
+			}
+			#endregion
+
+			#region Exercise 5
+			Exercises.Title ("5");
+			if (YesNo ()) {
+				Console.WriteLine ("please input min integer and max integer separated by a space");
+				string input = Console.ReadLine ();
+				string[] inputSplit = input.Split (new char[] { ' ' }, 2);
+				int minLimit = Convert.ToInt32 (inputSplit [0]);
+				int maxLimit = Convert.ToInt32 (inputSplit [1]);
+
+				int[] results = new int[(maxLimit - minLimit) - 1];
+				int resultsCounter = 0;
+
+				for (int i = 1; i < (maxLimit - minLimit); i++) {
+					if ((minLimit + i) % 5 == 0) {
+						resultsCounter++;
+						results [i - 1] = minLimit + i;
+					}
+				}
+				bool firstTicker = false;
+				if (resultsCounter > 0)
+					Console.WriteLine ("There are {0} numbers divisible by 5 between {1} and {2}, here they are:", resultsCounter, minLimit, maxLimit);
+				else
+					Console.WriteLine ("There are no numbers divisble by 5 between {0} and {1}", minLimit, maxLimit);
+				for (int i = 0; i < results.Length;i++) {
+					if (results [i] != 0) {
+						if (firstTicker)
+							Console.Write (", ");
+						firstTicker = true;
+						Console.Write (results [i]);
+					}
+					if (i == results.Length - 1)
+						Console.Write ("." + Environment.NewLine);
+			}
+				}
+			#endregion
+
+			#region Exercise 6
+			Exercises.Title("6");
+			if (YesNo()){
+				Console.Write("Enter 2 numbers to compare" + Environment.NewLine + "Number 1: ");
+				double[] nums = new double[2];
+				nums[0] = Exercises.ReadDouble();
+				Console.Write("Number 2: ");
+				nums[1] = Exercises.ReadDouble();
+				if (nums[0] == nums[1])
+					Console.WriteLine("These numbers are the same");
+				else
+					Console.WriteLine("{1} is greater than {2}, {0}and their difference is:{0}{3}", Environment.NewLine, Math.Max(nums[0], nums[1]), Math.Min(nums[0], nums[1]), Math.Max(nums[0], nums[1]) - Math.Min(nums[0], nums[1]));
+			}
+				#endregion
+
+
+			#region Exercise 7
+			Exercises.Title("7");
+			if (YesNo()){
+				Console.WriteLine("Input 5 numbers please, their sum  will be computed.");
+				double[] nums = new double[5];
+				double sum = 0;
+				for (int i = 1; i <= 5; i++) {
+					Console.Write("Number {0}",i);
+					nums[i - 1] = Exercises.ReadDouble();
+					sum += nums[i - 1];
+				}
+				Console.WriteLine("The sum of these numbers is:" + Environment.NewLine + sum);
+			}
+			#endregion
+
+			#region Exercise 8
+			Exercises.Title("8");
+			if (YesNo()){
+				Console.WriteLine("Input numbers to compare and press Enter when you're finished");
+				List<double> nums = new List<double>();
+				// take input
+				short count = 0;
+				while (true) {
+					count++;
+					Console.Write("> ");
+					double num;
+					string input = Console.ReadLine();
+						if (Double.TryParse(input, out num))
+							nums.Add(num);
+						else
+							break;
+				}
+				// calculate sum
+				double sum = 0;
+				if (nums.Count > 0){
+					for (int i = 0; i < nums.Count;i++) {
+						sum += nums[i];
+					}
+				}
+				Console.WriteLine("The sum of the numbers you input is: " + Environment.NewLine + sum);
+			}
+			#endregion
+
+		}
+
+
+
+
+
+		}
+	}
+
+
---
+++ MITAlgorithmsIntroduction.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FundamentalsOfCExercisesVariablesChapter
+{
+	public class MITAlgorithmsIntroduction
+	{
+
+		public static bool IsThereAPeak (float[] input)
+		{
+			int index;
+			return IsThereAPeak(input, out index);
+		}
+
+		#region IsThereAPeak
+		public static bool IsThereAPeak(float[] input, out int index)
+		{
+			bool result = false;
+			int length = input.Length;
+			int divideLimit = 0; //Number of times you can divide the array.
+			while (length >= 3) {
+				length /= 2;
+				divideLimit++;
+			}
+			length = input.Length;
+			Console.WriteLine ("divideLimit = " + divideLimit);//DEBUG
+
+			int range = length / 2;
+			index = range;
+			int offset = 0;
+			int p = 1;
+
+			Console.WriteLine ("input.Length = " + input.Length);//DEBUG
+			while(index <= input.Length && index >= 0) {
+				if (index == 0 || input [index] < input [index - 1]) {
+					if (p < divideLimit) {
+						index = range + offset;
+						range /= 2;
+						p++;
+						Console.WriteLine ("i < i - 1,  p=" + p + "   range=" + range + "   offset=" + offset +  "   index=" + index + "    value=" + input[index]);//DEBUG
+					} else {
+						if (input [index] > input [index + 1] && index == 0) {
+							Console.WriteLine ("i > i + 1,  p=" + p + "   range=" + range + "   offset=" + offset + "   index=" + index + "    value=" + input[index]);//DEBUG
+							result = true;
+							break;
+						}
+						index -= 1; 
+					}
+				} else if (input [index] > input [index - 1]) {
+					if (p < divideLimit) {
+						index = range + offset;
+						offset += range;
+						range /= 2;
+						p++;
+						Console.WriteLine ("i > i - 1,  p=" + p + "   range=" + range + "   offset=" + offset + "   index=" + index + "    value=" + input[index]);//DEBUG
+					} else {
+						Console.WriteLine ("yo");//DEBUG
+						if (index == (input.Length - 1)) {
+							Console.WriteLine ("Allelujah");//DEBUG
+							result = true;
+							break;
+						}
+						Console.WriteLine ("index {0}, input.Length {1}", index, input.Length);
+						if (index != (input.Length - 1)) {
+							Console.WriteLine (index + "      " + (index + 1));
+							Console.WriteLine (index + "      " + (input [index + 1]));
+							if (input [index] > input [index + 1]) {
+								Console.WriteLine ("i < i + 1,  p=" + p + "   range=" + range + "   offset=" + offset + "   index=" + index + "    value=" + input [index]);//DEBUG
+								result = true;
+								break;
+							}
+						}
+						index += 1;
+					}
+				}
+			}
+
+			Console.WriteLine ("main loop end");//DEBUG
+			return result;
+
+		}
+		#endregion
+
+
+		public static void Exercises ()
+		{
+
+			List<float[]> testArrays = new List<float[]>();
+			testArrays.Add(new float[] { 46f, 1f, 0f, -20f, 10.555f, 11f, 0f, 2f, 2.5f, 3f, 2f, 5f, 6.13f, 356f });
+			testArrays.Add(new float[] { 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10f, 11, 12, 13, 12});
+			testArrays.Add(new float[] {22, 12, -4f, -10, -50, -1230, -24556, -999999});
+			testArrays.Add(new float[] {1, 3, 5});
+
+
+
+			bool peak;
+			int peakIndex = -555;
+
+			for (int i = 0; i < testArrays.Count; i++) {
+
+				peak = IsThereAPeak (testArrays[i], out peakIndex);
+
+				Console.WriteLine ("There {0} a peak{1}", peak ? "is" : "isn't", peak ? " in position " + peakIndex + " and of value " + testArrays[i][peakIndex] : "");
+				Console.WriteLine ("---------------------------------------");
+			}
+		}
+	}
+}
+
