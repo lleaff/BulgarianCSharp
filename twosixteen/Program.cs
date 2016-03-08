bdcc913c3692581b4214d84a5534c89278137659
lleaff <lleaff@users.noreply.github.com>
2016-03-08 21:05:20 +0100

    +extension methods

--- twosixteen/Program.cs
+++ twosixteen/Program.cs
@@ -4,19 +4,19 @@ namespace TwoSixteen
 {
 	class MainClass
 	{
-		public static int Main (string[] args)
+		public static int Main(string[] args)
 		{
 			string name;
 			if (args.Length < 1) {
-				Console.WriteLine ("What's you name boy?");
+				Console.WriteLine("What's you name boy?");
 				if (String.IsNullOrEmpty(name = Console.ReadLine())) {
 					throw new Exception("I NEED A NAME GODAMIT");
 				}
 			} else {
-				name = args [0];
+				name = args[0];
 			}
-			name = char.ToUpper(name[0]) + name.Substring(1);
-			Console.WriteLine ("Hello {0}", name);
+			name = name.Capitalize();
+			Console.WriteLine("Hello {0}", name);
 			return 0;
 		}
 	}
---
+++ twosixteen/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoSixteen
+{
+	public static class StringExtensions
+	{
+		public static string Capitalize(this String str)
+		{
+			return Char.ToUpper(str[0]) + str.Substring(1);
+		}
+	}
+}
